c786179feabc0a66dab4750f9ebad0c71f0efc6c
baris_aksay <barish.aksay@gmail.com>
2025-01-09 15:27:51 +0100

    properties and growUp method added, tests are added

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,44 @@
 const Pet = require("../pet")
 
 describe('pet constructor tests', () => { 
+    let fido;
+    beforeEach(()=>{
+       fido = new Pet("fido")
+    })
+
     it("should create an instance of Pet",()=>{
-        let testPet= new Pet("petto")
-        expect(testPet).toBeInstanceOf(Pet)
+        expect(fido).toBeInstanceOf(Pet)
+    })
+
+    it("should set name prop",()=> {
+        expect(fido.name).toBe("fido")
+    })
+
+    it("should have initial age=0",()=>{
+        expect(fido.age).toBe(0)
+    })
+
+    it("should have initial fitness 10",()=>{
+        expect(fido.fitness).toBe(10);
+    })
+
+    it("should have initial hunger 0",()=>{
+
+        expect(fido.hunger).toBe(0);
+    })
+
+    it("growUp should increment age by 1 ",()=>{
+        fido.growUp()
+        expect(fido.age).toBe(1)
+    })
+
+    it("growUp should increase hunger by 5",()=>{
+        fido.growUp()
+        expect(fido.hunger).toBe(5);
+    })
+
+    it("growUp should decrease fitness by 3",()=>{
+        fido.growUp()
+        expect(fido.fitness).toBe(7);
     })
  })
